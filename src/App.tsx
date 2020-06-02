74a2fb9d7d6398e311c5288462cd247bcd054e9d
Natasha Lawson <natasha.lawson93@gmail.com>
2020-06-02 17:03:25 -0600

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,117 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styles from "./App.module.css";
 
 function App() {
+  const [operation, setOperation] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={styles.calculator}>
+      <div className={styles.display}>{operation}</div>
+      <div className={styles.keypad}>
+        <button onClick={(e) => setOperation("")} className={styles.clear}>
+          AC
+        </button>
+        <button
+          onClick={(e) => setOperation((operation) => operation + " / ")}
+          className={styles.operator}
         >
-          Learn React
-        </a>
-      </header>
+          /
+        </button>
+        <button
+          onClick={(e) => setOperation((operation) => operation + " * ")}
+          className={styles.operator}
+        >
+          x
+        </button>
+        <button
+          onClick={(e) => setOperation((operation) => operation + "7")}
+          className={styles.number}
+        >
+          7
+        </button>
+        <button
+          onClick={(e) => setOperation((operation) => operation + "8")}
+          className={styles.number}
+        >
+          8
+        </button>
+        <button
+          onClick={(e) => setOperation((operation) => operation + "9")}
+          className={styles.number}
+        >
+          9
+        </button>
+        <button
+          onClick={(e) => setOperation((operation) => operation + " - ")}
+          className={styles.operator}
+        >
+          -
+        </button>
+        <button
+          onClick={(e) => setOperation((operation) => operation + "4")}
+          className={styles.number}
+        >
+          4
+        </button>
+        <button
+          onClick={(e) => setOperation((operation) => operation + "5")}
+          className={styles.number}
+        >
+          5
+        </button>
+        <button
+          onClick={(e) => setOperation((operation) => operation + "6")}
+          className={styles.number}
+        >
+          6
+        </button>
+        <button
+          onClick={(e) => setOperation((operation) => operation + " + ")}
+          className={styles.operator}
+        >
+          +
+        </button>
+        <button
+          onClick={(e) => setOperation((operation) => operation + "1")}
+          className={styles.number}
+        >
+          1
+        </button>
+        <button
+          onClick={(e) => setOperation((operation) => operation + "2")}
+          className={styles.number}
+        >
+          2
+        </button>
+        <button
+          onClick={(e) => setOperation((operation) => operation + "3")}
+          className={styles.number}
+        >
+          3
+        </button>
+        <button
+          onClick={(e) => {
+            setOperation((operation) =>
+              String(eval(operation.replace(/^0+/, "")))
+            );
+          }}
+          className={styles.equals}
+        >
+          =
+        </button>
+        <button
+          onClick={(e) => setOperation((operation) => operation + "0")}
+          className={styles.zero}
+        >
+          0
+        </button>
+        <button
+          onClick={(e) => setOperation((operation) => operation + ".")}
+          className={styles.number}
+        >
+          .
+        </button>
+      </div>
     </div>
   );
 }
